dce8407426cadb0688777e50d90eedb331bd85b6
ana.cacosta1 <ana.cacosta1@CAS555596W0750.senacsp.edu.br>
2017-03-21 22:14:51 -0300

    Código Perímetro de teste 2

--- src/celsius/Celsius.java
+++ src/celsius/Celsius.java
@@ -6,13 +6,12 @@ import java.util.Scanner;
 public class Celsius {
 
     public static void main(String[] args) {
-        double f, c, resultado;
+        double f, c;
         System.out.println("Digite a temperatura em Fahrenheit: ");
         Scanner scan = new Scanner(System.in);
         f = scan.nextDouble();
        
         c = (f-32)/1.8;
-        resultado = c;
         
         System.out.println("Celsius: "+c);
     }
