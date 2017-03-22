b389ae9b52a70e1c20cc561341d9ea3dc2c0f059
ana.cacosta1 <ana.cacosta1@CAS555596W0750.senacsp.edu.br>
2017-03-21 21:55:49 -0300

    Primeiro Commit

---
+++ src/celsius/Celsius.java
@@ -0,0 +1,20 @@
+package celsius;
+
+import java.util.Scanner;
+
+
+public class Celsius {
+
+    public static void main(String[] args) {
+        double f, c;
+        System.out.println("Digite a temperatura em Fahrenheit: ");
+        Scanner scan = new Scanner(System.in);
+        f = scan.nextDouble();
+        
+        
+        c = (f-32)/1.8;
+        
+        System.out.println("Celsius: "+c);
+    }
+    
+}
